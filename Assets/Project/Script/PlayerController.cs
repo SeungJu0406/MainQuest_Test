5206c18bff7bb0e98c1d1c168342f1e5325cd5ac
Keinex <nss8164@gmail.com>
2026-05-15 15:44:18 +0900

    [Fix] 플레이어 컨트롤러에 직접 움직임 로직 추가

--- Assets/Project/Script/PlayerController.cs
+++ Assets/Project/Script/PlayerController.cs
@@ -1,45 +1,53 @@
 using Fusion;
+using System;
 using UnityEngine;
 
 public class PlayerController : NetworkBehaviour
 {
-    PlayerPhysicController _physic;
+    [SerializeField] private float _moveSpeed = 5f; // 이동 속도
 
+    Vector2 _moveDir;
+    Rigidbody2D _rb;
 
+    //GameObject _moveSuppoter;
     public override void Spawned()
     {
         if (HasStateAuthority == false) return;
 
-        _physic = GetComponentInChildren<PlayerPhysicController>();
+        _rb = GetComponent<Rigidbody2D>();
 
-        _physic.IsMyPlayer = true; // 플레이어 설정
-        _physic.transform.SetParent(null); //부모 나가기
-        transform.position = _physic.transform.position; // 초기 위치를 플레이어 위치로
+        //_moveSuppoter = new GameObject("MoveSupporter");
     }
 
+    private void Update()
+    {
+        if (HasStateAuthority == false) return;
+        InputPlayer();
+    
+    }
     public override void FixedUpdateNetwork()
     {
-        if(HasStateAuthority == false) return;
+        if (HasStateAuthority == false) return;
         MovePlayer();
     }
+
+    private void InputPlayer()
+    {
+        _moveDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+    }
+
 
     /// <summary>
     /// 플레이어 이동
     /// </summary>
     void MovePlayer()
     {
-        if (HasStateAuthority)
-        {
-            // 내 플레이어면 내 플레이어 모델이 물리처리 오브젝트 위치로
-            transform.position = _physic.transform.position;
-            transform.rotation = _physic.transform.rotation;
-        }
-        else
-        {
-            // 내 플레이어가 아니면 물리처리 오브젝트가 플레이어 모델 위치로
-            _physic.transform.position = transform.position;
-            _physic.transform.rotation = transform.rotation;
-        }
+        _rb.linearVelocity = Vector2.zero;
+        if(_moveDir == Vector2.zero) return;
+
+        Vector2 moveDir = transform.right * _moveDir.x + transform.up * _moveDir.y;
+        moveDir.Normalize();
+        _rb.linearVelocity = moveDir * _moveSpeed;
     }
 
 }
