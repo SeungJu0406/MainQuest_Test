68421a01da5c4a776b0943bf7a3396ba9ee89143
Keinex <nss8164@gmail.com>
2026-05-16 18:15:54 +0900

    [chore] 안쓰는 클래스 제거 및 폴더 위치 변경

--- Assets/Project/Script/Player/Controller/PlayerMoveModule.cs
+++ Assets/Project/Script/Player/Controller/PlayerMoveModule.cs
@@ -1,11 +1,29 @@
 using UnityEngine;
 
+[System.Serializable]
 public class PlayerMoveModule
 {
-    private PlayerController _controller;
+    [SerializeField] private float _moveSpeed = 5f;
 
-    public PlayerMoveModule(PlayerController controller)
+    private Rigidbody2D _rb;
+    private Vector2 _moveDir;
+
+    public int FacingDirX { get; private set; } = 1;
+
+    public void Init(Rigidbody2D rb)
     {
-        _controller = controller;
+        _rb = rb;
+    }
+
+    public void ReadInput()
+    {
+        _moveDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        if (_moveDir.x != 0)
+            FacingDirX = _moveDir.x > 0 ? 1 : -1;
+    }
+
+    public void ApplyMovement()
+    {
+        _rb.linearVelocity = _moveDir * _moveSpeed;
     }
 }
